a18df3cc9dd65b3040d5cb57b7289d935425d019
vyaesop <100709552+vyaesop@users.noreply.github.com>
2025-12-05 12:13:05 +0300

    CI: run mypy on 'ideeza' package; fix ruff import issues in services.py

--- ideeza/settings.py
+++ ideeza/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY") or secrets.token_urlsafe(50)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS: list[str] = []
 
 # Application definition
 
